5ba8d3f6f86ead23e8c097b6dbcceefa7c823034
QJ <quangjunior365@gmail.com>
2021-12-28 21:46:40 +0700

    Add project files.

---
+++ BookShopAPI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookShopAPI.Startup))]
+namespace BookShopAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

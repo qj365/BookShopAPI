78b70dbe41d88deaca1c79531fa9d311c9b9692e
vulong26 <77488668+vulong26@users.noreply.github.com>
2022-01-03 19:16:58 +0700

    nxb

---
+++ BookShopAPI/Areas/Admin/Controllers/PublisherController.cs
@@ -0,0 +1,46 @@
+﻿using BookShopAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookShopAPI.Areas.Admin.Controllers
+{
+    public class PublisherController : Controller
+    {
+        // GET: Admin/Publisher
+
+        private ApplicationDbContext _context;
+
+        public PublisherController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Admin/Author
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var publisher = _context.Publisher.SingleOrDefault(c => c.Id == id);
+            if (publisher == null)
+                return HttpNotFound();
+            return View(publisher);
+        }
+    }
+}

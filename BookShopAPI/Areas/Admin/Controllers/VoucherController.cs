367a8c8c6613ef404120c2522c8cabb5fa894463
TrungDat <77280631+TrungDat@users.noreply.github.com>
2022-01-03 17:24:55 +0700

    khuyen mai

---
+++ BookShopAPI/Areas/Admin/Controllers/VoucherController.cs
@@ -0,0 +1,43 @@
+﻿using BookShopAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookShopAPI.Areas.Admin.Controllers
+{
+    public class VoucherController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public VoucherController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Admin/Voucher
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var voucher = _context.Voucher.SingleOrDefault(c => c.Id == id);
+            if (voucher == null)
+                return HttpNotFound();
+            return View(voucher);
+        }
+    }
+}
--- BookShopAPI/Models/Voucher.cs
+++ BookShopAPI/Models/Voucher.cs
@@ -1,4 +1,4 @@
-namespace BookShopAPI.Models
+﻿namespace BookShopAPI.Models
 {
     using System;
     using System.Collections.Generic;
@@ -15,17 +15,36 @@ namespace BookShopAPI.Models
             Orders = new HashSet<Orders>();
         }
 
+        [Key]
+        [Display(Name = "Mã khuyến mãi")]
         public int Id { get; set; }
 
+        [Display(Name = "Phần trăm khuyến mãi")]
+        [Required(ErrorMessage = "Vui lòng nhập phần trăm khuyến mãi")]
+        [Range(0, int.MaxValue, ErrorMessage = "Vui lòng nhập giá trị lớn hơn 0")]
         public int Discount { get; set; }
 
+        [Column(TypeName = "date")]
+        [Display(Name = "Ngày bắt đầu")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [Required(ErrorMessage = "Vui lòng nhập ngày bắt đầu")]
         public DateTime StartDate { get; set; }
 
+        [Column(TypeName = "date")]
+        [Display(Name = "Ngày kết thúc")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [Required(ErrorMessage = "Vui lòng nhập ngày kết thúc")]
         public DateTime EndDate { get; set; }
 
-        [Required]
         [StringLength(50)]
+        [Display(Name = "Tên khuyến mãi")]
+        [Required(ErrorMessage = "Vui lòng nhập tên khuyến mãi")]
         public string Name { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Mã code khuyến mãi")]
+        [Required(ErrorMessage = "Vui lòng nhập code khuyến mãi")]
+        public string Code { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Orders> Orders { get; set; }
